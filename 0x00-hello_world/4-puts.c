b81a46a1e162b91f1797d531bdf8626eedf9d723
BrianKvin <briankvin@gmail.com>
2022-09-09 06:49:06 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * main- Entry point
+ *main- Entry point
  *
- * Return: Success
-*/
+ *Return: Success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzie");
