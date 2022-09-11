7240f1f9c724262a61d712dfd8574c0f1d3fe076
BrianKvin <briankvin@gmail.com>
2022-09-11 13:39:28 -0700

    8 C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 /**
- *
- *  *main - Entry point
- *
- *   *
- *
- *    *Return: Always 0 (Success)
- *
- *    */
+ * main - Entry point
+ * Return: 1
+ */
 
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t len = strlen(str);
-	/*Starting main*/
-	fwrite(str, len, 1, stderr);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
