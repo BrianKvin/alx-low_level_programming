9b8c91879e85d2ffc2bb24ec7166b0092250d51e
BrianKvin <briankvin@gmail.com>
2022-09-12 12:31:22 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - positive anything is better than nothing
+ * Describe: print positive, zero, or negative numbers
+ * this progrm prints positive, zero or negative numbers
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
