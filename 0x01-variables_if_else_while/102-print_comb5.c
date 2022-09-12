ff4af5949b5ed208d9510853441e8e8274770a0f
BrianKvin <briankvin@gmail.com>
2022-09-12 16:36:30 -0700

    Comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-	int i, e , g, h, op1, op2;
+	int i,e,g,h,op1,op2;
+
 	i = e = g = h = 48;
-	while (h <58)
+	while (h < 58)
 	{
-		g= 48;
+		g = 48;
 		while (g < 58)
 		{
 			e = 48;
