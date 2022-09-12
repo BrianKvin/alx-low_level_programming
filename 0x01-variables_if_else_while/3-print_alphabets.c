6d6a7e8657d892750895c9f7fc69e52ed84d5079
BrianKvin <briankvin@gmail.com>
2022-09-12 05:57:31 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 when successful
+ */
+int main(void)
+	char minuscules = 'a';
+	char mayusculas = 'A';
+	char retorno = '\n';
+	while (minusculas <= 'z')
+{
+	putchar (minusculas);
+	minusculas++;
+}
+
+while (mayusculas <= 'z')
+{
+	putchasr (mayusculas);
+	mayusculas++
+}
+putchar (retorno);
+return (0);
