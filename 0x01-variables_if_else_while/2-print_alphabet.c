3deabadd6f2961f7cb972382b0412cc2c0616143
BrianKvin <briankvin@gmail.com>
2022-09-12 05:24:05 -0700

    Alphabet letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/** main -print a letter of the aplhabet
+ *
+ * Description: print letters of the alphabet
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letra = 'a';
+	char retorno = '\n';
+	while (letra <= 'z')
+	{
+		putchar (letra);
+		letra++;
+	}
+	putchar (retrono);
+	return (0);
+}
