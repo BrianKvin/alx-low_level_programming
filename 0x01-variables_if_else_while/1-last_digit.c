1846dbdf5bd79e2a0a044812697157ced0b288e5
BrianKvin <briankvin@gmail.com>
2022-09-12 05:02:46 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time,h>
+#include <time.h>
 /**
- * main - principla function
+ * main - print last digit in random number
  *
  * Return: end the program
  */
