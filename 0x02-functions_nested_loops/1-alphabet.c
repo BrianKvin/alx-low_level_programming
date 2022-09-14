358e1495c5b3358dd9610d1b78ddbb026b6e497a
BrianKvin <briankvin@gmail.com>
2022-09-14 14:00:04 -0700

    printAlpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * printAlpha - prints alphabet in lower case
  *
- * Return: 0
+ * Return: void
  */
 void printAlpha(void)
 {
 	int i;
 
-	for (i = 'a'; i<= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == 'c' || i == 'k')
 			_putchar(i);
