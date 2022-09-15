5a826e2ea2ecc7e30ce28ce9cb341e6c9eb97965
BrianKvin <briankvin@gmail.com>
2022-09-14 19:15:45 -0700

    Numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,9 @@
 #include "main.h"
 /**
  * print_to_98 -prints all numbers from input to 98,
- * 	numbers separated by comma followed by space
+ * numbers separated by comma followed by space
  * @n: The first number to begin counting from
+ * Return: Void
  */
 void print_to_98(int n)
 {
