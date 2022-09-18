cef8d50a1c5de2b14d29973f90081a3dc0951445
BrianKvin <briankvin@gmail.com>
2022-09-18 03:13:01 -0700

    correction

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int a = 0, b = 1, next = 0;
+	int a = 0, b = 1, second = 0;
 	int sum = 0;
 
 	while (next < 4000000)
 	{
 		next = a + b;
 		a = b;
-		b = next;
-		if (next % 2 == 0)
-			sum += next;
+		b = second;
+		if (second % 2 == 0)
+			sum += second;
 	}
 	printf("%i\n", sum);
 	return (0);
