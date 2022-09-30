723a4a712fbcafef61bc0c72da288df5c7306dd3
BrianKvin <briankvin@gmail.com>
2022-09-30 15:15:38 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if (argc == 1)
+	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		printf("%d\n", a * b);
 		return (0);
 	}
-	else
-	printf("error\n");
+	if (argc != 3)
+	printf("%s\n", "Error");
 	return (1);
 }
