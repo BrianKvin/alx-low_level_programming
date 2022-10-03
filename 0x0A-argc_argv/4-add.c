15c2869849d6a86fe58ef56d3aeee6992efba8c6
BrianKvin <briankvin@gmail.com>
2022-10-03 06:59:31 -0700

    correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 1; j < argc; j++)
+		for (j = 0; j < argc; j++)
 			if (isdigit(argv[i][j]) == 0)
 			{
 			puts("Error");
